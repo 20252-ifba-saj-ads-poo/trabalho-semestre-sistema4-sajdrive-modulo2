f29a4724181f12703b66d3134e08f51caefd6977
jv002152 <jv002152@gmail.com>
2026-03-02 17:12:32 -0300

    Ajustes

--- src/main/java/br/edu/ifba/saj/fwads/model/Linha.java
+++ src/main/java/br/edu/ifba/saj/fwads/model/Linha.java
@@ -8,7 +8,6 @@ public class Linha {
     private int id;
     private String nome;    
     private List<Ponto> listaDePontos;
-    private String sentido;
     private List<Viagem> listaDeViagens;
 
 }
